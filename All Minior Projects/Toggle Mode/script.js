4ea14577132b89d0c5b7a7530ca6b93b9c675639
Bharath2025 <mbharath@karunya.edu.in>
2022-02-13 20:51:08 +0530

    Improved the Toggle Project JS code.

--- All Minior Projects/Toggle Mode/script.js
+++ All Minior Projects/Toggle Mode/script.js
@@ -2,11 +2,22 @@ var toggle_btn=document.getElementById('toggle');
 var toggle_circle=document.getElementById('toggle-indicator');
 var body=document.getElementsByTagName('*');
 
+var count=0;
+
 toggle_btn.addEventListener('click',function(){
 
     //Toggle property has to be implemented here.
-    body[0].style.backgroundColor="black";
-    body[0].style.color="white";
-    toggle_circle.style.left="75%";
-    toggle_btn.style.border = "2px solid white";
+    count++;
+    if(count%2 == 0){
+        body[0].style.backgroundColor="white";
+        body[0].style.color="black";
+        toggle_circle.style.left="-5%";
+        toggle_btn.style.border = "2px solid black";
+    }
+    else{
+        body[0].style.backgroundColor="black";
+        body[0].style.color="white";
+        toggle_circle.style.left="75%";
+        toggle_btn.style.border = "2px solid white";
+    }
 });
